9f215076d34739c72085aae326f718617cbd740f
Samira Gibson <saga44studio@gmail.com>
2023-09-13 22:48:03 -0400

    chariot solutions logo added.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-/* Create event listener for buttons with id menuItem in the nav tag */
-/* If a button is clicked it should have an active class property and the other buttons with menuItem id
- should have this property removed*/
+/*Event listener for changing nav button style based on state */
 
 const menuItem = document.querySelectorAll(".menuItem").forEach(item => {
     item.addEventListener("click", () => {
